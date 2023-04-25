fc0875752b4513ffcbcf3217c3c158965697dc03
Diego Arreceygor <darreceygor@gmail.com>
2023-04-25 15:38:10 -0300

    fin de entregables TP # 3

--- Clase 3. Transformaciones y animaciones/practico3/js/index.js
+++ Clase 3. Transformaciones y animaciones/practico3/js/index.js
@@ -35,3 +35,41 @@ function segundos() {
     segundos();
   }, 1000);
 })();
+
+
+const $card = document.querySelector('.card');
+let bounds;
+
+function rotateToMouse(e) {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  const leftX = mouseX - bounds.x;
+  const topY = mouseY - bounds.y;
+  const center = {
+    x: leftX - bounds.width / 2,
+    y: topY - bounds.height / 2,
+  }
+  const distance = Math.sqrt(center.x**2 + center.y**2);  /*distancia entre el centro y el mouse*/
+  
+  $card.style.transform = `
+    scale3d(1.1, 1.1, 1.1)
+    rotate3d(
+      ${center.y / 100},
+      ${-center.x / 100},
+      0,
+      ${Math.log(distance)* 8}deg
+    )
+  `;
+}
+
+$card.addEventListener('mouseenter', () => {
+  bounds = $card.getBoundingClientRect();
+  document.addEventListener('mousemove', rotateToMouse);
+});
+
+$card.addEventListener('mouseleave', () => {
+  document.removeEventListener('mousemove', rotateToMouse);
+  $card.style.transform = '';
+  $card.style.background = '';
+});
